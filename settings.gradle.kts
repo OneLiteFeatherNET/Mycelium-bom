86adedbb7f3cb5a6007262c40e12bcacf36fa809
theEvilReaper <steffenwx@gmail.com>
2025-03-17 15:21:07 +0100

    Refactor dependency handling and usage

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,28 @@
 rootProject.name = "mycelium-bom"
 
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+    versionCatalogs {
+        create("libs") {
+            version("microtus", "1.5.0")
+            version("adventure", "4.19.0")
+            version("junit", "5.11.3")
+            version("mockito", "5.14.2")
+            version("shadow", "8.3.6")
+
+            library("microtus", "net.onelitefeather.microtus", "Microtus").versionRef("microtus")
+            library("microtus-testing", "net.onelitefeather.microtus.testing", "testing").versionRef("microtus")
+            library("adventure-text-minimessage", "net.kyori", "adventure-text-minimessage").versionRef("adventure")
+            library("junit-jupiter-api", "org.junit.jupiter", "junit-jupiter-api").versionRef("junit")
+            library("junit-jupiter-engine", "org.junit.jupiter", "junit-jupiter-engine").versionRef("junit")
+            library("mockito-core", "org.mockito", "mockito-core").versionRef("mockito")
+            library("mockito-junit-jupiter", "org.mockito", "mockito-junit-jupiter").versionRef("mockito")
+
+            plugin("shadow", "com.gradleup.shadow").versionRef("shadow")
+        }
+    }
+}
+
 include("base")
