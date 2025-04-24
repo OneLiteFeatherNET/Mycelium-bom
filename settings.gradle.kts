0c7efa41138ffa0f3455e1721f92d8ed3df9cd20
theEvilReaper <steffenwx@gmail.com>
2025-04-24 19:15:45 +0200

    Switch from Microtus to Minestom

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,13 +6,12 @@ dependencyResolutionManagement {
     }
     versionCatalogs {
         create("libs") {
-            version("microtus", "1.5.1")
+            version("minestom", "a1d1920a04")
             version("adventure", "4.20.0")
             version("junit", "5.12.2")
             version("mockito", "5.17.0")
 
-            library("microtus", "net.onelitefeather.microtus", "Microtus").versionRef("microtus")
-            library("microtus-testing", "net.onelitefeather.microtus.testing", "testing").versionRef("microtus")
+            library("minestom","net.minestom", "minestom-snapshots").versionRef("minestom")
             library("adventure-text-minimessage", "net.kyori", "adventure-text-minimessage").versionRef("adventure")
             library("junit-jupiter-api", "org.junit.jupiter", "junit-jupiter-api").versionRef("junit")
             library("junit-jupiter-engine", "org.junit.jupiter", "junit-jupiter-engine").versionRef("junit")
