564d737de672c1057e578a27e276a355c5756c98
theEvilReaper <steffenwx@gmail.com>
2025-04-29 12:23:23 +0200

    Include more junit dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -24,14 +24,21 @@ dependencyResolutionManagement {
             version("minestom", "a1d1920a04")
             version("adventure", "4.20.0")
             version("junit", "5.12.2")
+            version("junit.platform", "1.12.2")
             version("mockito", "5.17.0")
             version("cyano", "0.1.0")
 
             library("minestom","net.minestom", "minestom-snapshots").versionRef("minestom")
             library("cyano", "net.onelitefeather.cyano", "cyano").versionRef("cyano")
-            library("adventure-text-minimessage", "net.kyori", "adventure-text-minimessage").versionRef("adventure")
-            library("junit-jupiter-api", "org.junit.jupiter", "junit-jupiter-api").versionRef("junit")
-            library("junit-jupiter-engine", "org.junit.jupiter", "junit-jupiter-engine").versionRef("junit")
+            library("adventure.minimessage", "net.kyori", "adventure-text-minimessage").versionRef("adventure")
+
+            // Junit libraries
+            library("junit.jupiter.api", "org.junit.jupiter", "junit-jupiter-api").versionRef("junit")
+            library("junit.jupiter.engine", "org.junit.jupiter", "junit-jupiter-engine").versionRef("junit")
+            library("junit.jupiter.params", "org.junit.jupiter", "junit-jupiter-params").versionRef("junit")
+            library("junit.platform.launcher", "org.junit.platform", "junit-platform-launcher").versionRef("junit.platform")
+
+            // Mock libraries
             library("mockito-core", "org.mockito", "mockito-core").versionRef("mockito")
             library("mockito-junit-jupiter", "org.mockito", "mockito-junit-jupiter").versionRef("mockito")
         }
