3a9eb5a9bcd46676c2401e6b3a737369cb9f5ac8
theEvilReaper <theEvilReaper@users.noreply.github.com>
2026-08-18 10:07:25 +0200

    chore(repo): add new maven url

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,21 +3,7 @@ rootProject.name = "mycelium-bom"
 dependencyResolutionManagement {
     repositories {
         mavenCentral()
-        maven {
-            name = "OneLiteFeatherRepository"
-            url = uri("https://repo.onelitefeather.dev/onelitefeather")
-            if (System.getenv("CI") != null) {
-                credentials {
-                    username = System.getenv("ONELITEFEATHER_MAVEN_USERNAME")
-                    password = System.getenv("ONELITEFEATHER_MAVEN_PASSWORD")
-                }
-            } else {
-                credentials(PasswordCredentials::class)
-                authentication {
-                    create<BasicAuthentication>("basic")
-                }
-            }
-        }
+        maven("https://repo.onelitefeather.dev/releases")
     }
     versionCatalogs {
         create("libs") {
